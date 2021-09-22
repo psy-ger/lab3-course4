184e11b1d0907828b759300bd5f0d073b599cd59
Bohdan_Novickyi <61537968+psy-ger@users.noreply.github.com>
2021-09-22 16:37:31 +0300

    second-lab3-course-full

--- lab3-course4/Program.cs
+++ lab3-course4/Program.cs
@@ -17,12 +17,10 @@ namespace Program
             Console.WriteLine(class_call1.formula_demo());
             Console.WriteLine(class_call1.formula_full());
             Console.WriteLine(class_call1.formula_full_program());
-            Program2_class2 class_call2 = new Program2_class2();
-            Console.WriteLine(class_call2.formula_num_demo());
-            Console.WriteLine(class_call2.formula_arctng_x());
-            Console.WriteLine(class_call2.formula_full2());
-            Program2_class3 class_call3 = new Program2_class3();
-            Console.WriteLine(class_call3.formula_t());
+            Console.WriteLine(class_call1.formula_num_demo());
+            Console.WriteLine(class_call1.formula_arctng_x());
+            Console.WriteLine(class_call1.formula_full2());
+            Console.WriteLine(class_call1.formula_t());
         }
 
     }
--- lab3-course4/Program2.cs
+++ lab3-course4/Program2.cs
@@ -9,32 +9,46 @@ namespace Program2
 {
     class Program2_class1
     {
-        public int x_int;
-        public int n_int;
-        public int sum_num;
-        public int sum_demo;
-        public int sum_full;
-        public int sum_full_formula;
+        public double x_int;
+        public double n_int;
+        public int sing_int;
+        public double t_int;
+        public double answer;
+        public double sum_num;
+        public double sum_demo;
+        public double sum_full;
+        public double sum_num_demo;
+        public double sum_arctng;
+        public double sum_arctng_full;
+        public double sum_formula_full2;
+        public double sum_full_formula;
         public string Enter_X_N()
         {
-            x_int = Convert.ToInt32(Console.ReadLine());
-            n_int = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Значение Х = ");
+            x_int = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Значение N = ");
+            n_int = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Кол-во знаков после запятой = ");
+            sing_int = Convert.ToInt32(Console.ReadLine());
             return "";
         }
         public string  formula_num()
         { 
-            sum_num = Convert.ToInt32(Math.Pow(x_int, 2 * n_int + 1));
+            sum_num = Convert.ToDouble(Math.Pow(x_int, 2 * n_int + 1));
+            //Console.WriteLine(sum_num);
             return "";
         }
         public string formula_demo()
         {
-            sum_demo = Convert.ToInt32(4 * Math.Pow(n_int, 2) - 1);
+            sum_demo = Convert.ToDouble((4 * Math.Pow(n_int, 2)) - 1);
+            //Console.WriteLine(sum_demo);
             return "";
         }
         public string formula_full()
         {
             sum_full = sum_num / sum_demo;
-            return Convert.ToString(sum_full);
+            //Console.WriteLine(sum_full);
+            return "";
         }
         public string formula_full_program()
         {
@@ -43,39 +57,34 @@ namespace Program2
                 sum_full_formula += sum_full;
 
             }
-            return Convert.ToString(sum_full_formula);
+            Console.WriteLine("Ряд = " + sum_full_formula);
+            return "";
         }
-        
-    }
-    class Program2_class2 : Program2_class1
-    {
-        public int sum_num_demo;
-        public int sum_arctng;
-        public int sum_formula_full2;
         public string formula_num_demo()
         {
-            sum_num_demo = Convert.ToInt32((1 + Math.Pow(x_int, 2))/2);
-            return Convert.ToString(sum_num_demo);
+            sum_num_demo = (1 + Math.Pow(x_int, 2)) / 2;
+            //Console.WriteLine(sum_num_demo);
+            return "";
         }
         public string formula_arctng_x()
-        {
-            sum_arctng = Convert.ToInt32(Math.Atan(x_int) - (x_int / 2));
-            return Convert.ToString(sum_arctng);
+        {          
+            sum_arctng = Math.Atan(x_int) - (x_int/2);
+            return "";
         }
         public string formula_full2()
         {
             sum_formula_full2 = sum_num_demo * sum_arctng;
-            return Convert.ToString(sum_formula_full2);
+            Console.WriteLine("Точное значение = " + sum_formula_full2);
+            return "";
         }
-    }
-    class Program2_class3 : Program2_class2
-    {
-        public int t_int;
         public string formula_t()
         {
-            t_int = Convert.ToInt32((Math.Abs(sum_full_formula - sum_formula_full2) / sum_formula_full2) * 100);
-            return Convert.ToString(t_int);
+            t_int = (Math.Abs(sum_full_formula - sum_formula_full2) / sum_formula_full2) * 100;
+            Console.WriteLine("Ответ Т = " + Math.Round(Convert.ToDecimal(t_int), sing_int));
+            return "";
         }
+
     }
+    
 
 }
